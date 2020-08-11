70f00b1840983c457ff1917b1f809a10d9fc8961
scottfleck0 <64764946+scottfleck0@users.noreply.github.com>
2020-08-11 15:01:25 +1200

    the character now falls with the effect of gravity
    i'm only uploading the js file as it is the only one which has been changed

--- flappy-bird-ish.js
+++ flappy-bird-ish.js
@@ -1,16 +1,48 @@
 const CVS = document.getElementById("gameCanvas");
 const CTX = CVS.getContext('2d');
 
+// constant for the height of the floor
 const floorHeight = 3 * (CVS.height / 4);
 
+// constant for gravity
+const gravity = 0.5;
+
+// variable containing colours
 var colours = {
   skyColour: "#33CCFF",
   groundColour: "#009900",
   characterColour: "#ffff00"
 }
 
-CTX.fillStyle = colours.skyColour;
-CTX.fillRect(0, 0, CVS.width, CVS.height);
+// variable containing all the character information
+var character = {
+  x: 100,
+  y: 100,
+  radius: 20,
+  y_velocity: 0
+}
 
-CTX.fillStyle = colours.groundColour;
-CTX.fillRect(0, floorHeight, CVS.width, CVS.height / 4);
+function draw() {
+
+  // drawing the sky
+  CTX.fillStyle = colours.skyColour;
+  CTX.fillRect(0, 0, CVS.width, CVS.height);
+
+  //drawing the background
+  CTX.fillStyle = colours.groundColour;
+  CTX.fillRect(0, floorHeight, CVS.width, CVS.height / 4);
+
+  // drawing the character
+  CTX.beginPath();
+  CTX.arc(character.x, character.y, character.radius, 0, Math.PI * 2);
+  CTX.fillStyle = colours.characterColour;
+  CTX.fill();
+
+  // changing the characters y velocity by gravity and then changing the characters y by the velocity
+  character.y_velocity += gravity;
+  character.y += character.y_velocity;
+
+  window.requestAnimationFrame(draw);
+};
+
+window.requestAnimationFrame(draw);
