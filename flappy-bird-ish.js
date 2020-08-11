27bd55a7d2bd0501145e5abc1137f7e72df26ef8
scottfleck0 <64764946+scottfleck0@users.noreply.github.com>
2020-08-11 14:19:05 +1200

    now draws sky and ground
    have added code to draw the sky and the ground, with a variable called colours which contains the colours for the game

--- flappy-bird-ish.js
+++ flappy-bird-ish.js
@@ -1,1 +1,16 @@
-console.log("working");
+const CVS = document.getElementById("gameCanvas");
+const CTX = CVS.getContext('2d');
+
+const floorHeight = 3 * (CVS.height / 4);
+
+var colours = {
+  skyColour: "#33CCFF",
+  groundColour: "#009900",
+  characterColour: "#ffff00"
+}
+
+CTX.fillStyle = colours.skyColour;
+CTX.fillRect(0, 0, CVS.width, CVS.height);
+
+CTX.fillStyle = colours.groundColour;
+CTX.fillRect(0, floorHeight, CVS.width, CVS.height / 4);
