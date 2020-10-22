7a311b62bf264a93970852b1b757c4a0ff5ce1c1
scottfleck0 <64764946+scottfleck0@users.noreply.github.com>
2020-10-23 09:11:25 +1300

    audio now imported and plays
    i found free to use audio files online and uploaded them into my code and set them to play at certain times throughout the gameplay

--- flappy-bird-ish.js
+++ flappy-bird-ish.js
@@ -1,68 +1,109 @@
+
 const CVS = document.getElementById("gameCanvas");
 const CTX = CVS.getContext('2d');
 
-// function to get a random value
-function randomValue(min, max) {
-  return Math.random() * (max - min) + min;
-}
 
 // variable for if the game is running or not
 var gameRunning = true;
 
+
+// function to get a random value
+function randomValue(min, max) {
+
+  return Math.random() * (max - min) + min;
+
+}
+
+
+// bringing in audio
+let jump = new Audio();
+jump.src = "Audio/jump.m4a";
+
+let point = new Audio();
+point.src = "Audio/point.m4a";
+
+let hit = new Audio();
+hit.src = "Audio/hitPipe.m4a";
+
+let dieSound = new Audio();
+dieSound.src = "Audio/die.mp3";
+
+
 // constant for the height of the floor
 const FLOORHEIGHT = 3 * (CVS.height / 4);
+
 
 // constants for jumping animation
 const GRAVITY = 0.35;
 const JUMPSTRENGTH = -6;
 
+
 // vars for score and lives
 var score = 0;
 var lives = 3;
 
-var scoreboard = []; // array for the scores which will be used for the scoreboard
+
+// array for the scores which will be used for the scoreboard
+var scoreboard = [];
+var scoreboardYPos = FLOORHEIGHT - 200; // making the y position variable for the scoreboard
+
 
 // arrays for the pipes, coins and floating obstacles
 var pipes = [];
 var coins = [];
 var obstacles = [];
 
+
 // speed for all the objects in the game
 const OBJSPEED = 3;
 
+
 // constants for floating items
 const FLOATCONSTS = {
+
   MINY: 50,
   MAXY: FLOORHEIGHT - 50
+
 }
+
 
 // variable containing colours
 var colours = {
+
   skyColour: "#33CCFF",
   groundColour: "#009900",
   characterColour: "#ffff00",
   pipesColour: "black",
   obsColour: "red"
+
 }
+
 
 // variable containing all the character information
 var character = {
+
   x: 100,
   y: 100,
   radius: 15,
   yVelocity: 0,
   jumping: false
+
 };
 
+
 const PIPECONSTS = {
+
   WIDTH: 20,
   GAPHEIGHT: 100,
   MINY: 30,
   MAXY: FLOORHEIGHT - 100 - 30,// the 100 is the gapheight but it theoretically isn't defined yet so I need to put 100 instead of the variable
-  DISTANCEBETWEEN: CVS.width / 2
+  DISTANCEBETWEEN: 250
+
 };
 
+
 var controller = {
+
   space: false,
   rKey: false,
 
@@ -85,33 +126,61 @@ var controller = {
   }
 };
 
+
 // function for collision detection
 function collisionDetection() {
   // collision detection for the pipes and the character
   for (var i = 0; i < pipes.length; i++) {
+
     if (character.x + character.radius > pipes[i].x && character.x - character.radius < pipes[i].x + PIPECONSTS.WIDTH) {// x axis
+
       if (character.y - character.radius * 0.8 < pipes[i].topY || character.y + character.radius * 0.8 > pipes[i].topY + PIPECONSTS.GAPHEIGHT) { // y axis
+
+        hit.play();
+        dieSound.play();
         lives = 0;
+
       }
     }
   }
 
   // collision detection for the floating obstacles and the character
   for (var i = 0; i < obstacles.length; i++) {
+
     if (character.x + character.radius * 0.8 > obstacles[i].x && character.x - character.radius * 0.8 < obstacles[i].x + obstacles[i].width) {// x axis
+
       if (character.y - character.radius * 0.8 < obstacles[i].y + obstacles[i].height && character.y + character.radius * 0.8 > obstacles[i].y) { // y axis
+
         lives -= 1;
+
+        if (lives <= 0) {
+
+          hit.play();
+          dieSound.play();
+
+        }else if (lives > 0){
+
+          hit.play();
+
+        }
+
         obstacles.splice(i,1);
+
       }
     }
   }
 
   // collision detection for the coins and the character
   for (var i = 0; i < coins.length; i++) {
+
     if (character.x + character.radius * 0.67 > coins[i].x - coins[i].radius * 0.67 && character.x - character.radius * 0.67 < coins[i].x + coins[i].radius * 0.67) { // if the coin and character line up on the x axis
+
       if (character.y - character.radius * 0.67 < coins[i].y + coins[i].radius * 0.67 && character.y + character.radius * 0.67 > coins[i].y - coins[i].radius * 0.67) { // if they line up on the y
+
         score += coins[i].value;
         coins.splice(i,1);
+        point.play();
+
       }
     }
   }
@@ -119,26 +188,36 @@ function collisionDetection() {
   // collision detection between the coins and obstacles
   for (var i = 0; i < obstacles.length; i++) {
     if (obstacles[i].x < coins[0].x + coins[0].radius && obstacles[i].x + obstacles[i].width > coins[0].x - coins[0].radius) {
-      if (obstacles[i].y - 20< coins[0].y + coins[0].radius && obstacles[i].y + obstacles[i].height + 20 > coins[0].y - coins[0].radius) {
+
+      if (obstacles[i].y - 20 < coins[0].y + coins[0].radius && obstacles[i].y + obstacles[i].height + 20 > coins[0].y - coins[0].radius) {
+
         obstacles.splice(i,1);
         makeObstacle();
+
       }
     }
   }
 }
 
+
 // function to make a new pipe
 function makePipe() {
+
   var pipe = {
     x: CVS.width,
     topY: randomValue(PIPECONSTS.MINY, PIPECONSTS.MAXY)
   }
+
   // unshift this new pipe into the front of the pipes array
   pipes.unshift(pipe);
+
 };
+
+
 
 // function to make a new coin
 function makeCoin() {
+
   var coinChoice = randomValue(0, 3);
 
   // variables for each type of coin
@@ -150,6 +229,7 @@ function makeCoin() {
     y: randomValue(FLOATCONSTS.MINY, FLOATCONSTS.MAXY)
   };
 
+
   var coin2 = {
     value: 5,
     colour: "silver",
@@ -158,6 +238,7 @@ function makeCoin() {
     y: randomValue(FLOATCONSTS.MINY, FLOATCONSTS.MAXY)
   };
 
+
   var coin3 = {
     value: 10,
     colour: "gold",
@@ -165,6 +246,7 @@ function makeCoin() {
     x: CVS.width + 10,
     y: randomValue(FLOATCONSTS.MINY, FLOATCONSTS.MAXY)
   };
+
 
   // unshift the selected coin into the front of the coins array
   if (coinChoice >= 0 && coinChoice < 1) {
@@ -187,6 +269,7 @@ function makeCoin() {
 
 // function to make a new obstacle
 function makeObstacle() {
+
   // variables for each size obs
   var obs = {
     lifeLost: 1,
@@ -201,54 +284,70 @@ function makeObstacle() {
 };
 
 
+
 function draw() {
 
+
   collisionDetection();
+
 
   // drawing the sky
   CTX.fillStyle = colours.skyColour;
   CTX.fillRect(0, 0, CVS.width, CVS.height);
 
+
   // drawing pipes
   for (var i = 0; i < pipes.length; i++) {
+
     CTX.fillStyle = colours.pipesColour;
     CTX.fillRect(pipes[i].x, pipes[i].topY, PIPECONSTS.WIDTH, - pipes[i].topY);
+
 
     CTX.fillStyle = colours.pipesColour;
     CTX.fillRect(pipes[i].x, pipes[i].topY + PIPECONSTS.GAPHEIGHT, PIPECONSTS.WIDTH, CVS.height - pipes[i].topY - PIPECONSTS.GAPHEIGHT);
 
+
     if (pipes[0].x < CVS.width - PIPECONSTS.DISTANCEBETWEEN) {
+
       // making a pipe when the frontmost pipe is at the set distance
       makePipe();
 
     } else if (pipes[i].x < - PIPECONSTS.WIDTH){
+
       // pop removes the last item in an array, which seeing as the pipes are added to the front of the array, the pop will remove the leftmost pipe.
       pipes.pop();
 
     } else {
+
       // moving the pipe
       pipes[i].x -= OBJSPEED;
+
     }
   }
 
   // drawing the coins
   for (var i = 0; i < coins.length; i++) {
+
     CTX.beginPath();
     CTX.arc(coins[i].x, coins[i].y, coins[i].radius, 0, Math.PI * 2);
     CTX.fillStyle = coins[i].colour;
     CTX.fill();
 
     if (coins[0].x - coins[0].radius < CVS.width - PIPECONSTS.DISTANCEBETWEEN) {
+
       // making a coin when the frontmost coin is at the set distance
       makeCoin();
 
     } else if (coins[i].x < - PIPECONSTS.WIDTH){
+
       // pop removes the last item in an array, which seeing as the coins are added to the front of the array, the pop will remove the leftmost coin.
       coins.pop();
 
     } else {
+
       // moving the coins
       coins[i].x -= OBJSPEED;
+
     }
   }
 
@@ -263,24 +362,30 @@ function draw() {
       makeObstacle();
 
     } else if (obstacles[i].x < - obstacles[i].width){
+
       // pop removes the last item in an array, which seeing as the obstacles are added to the front of the array, the pop will remove the leftmost obs.
       obstacles.pop();
 
     } else {
+
       // moving the obs
       obstacles[i].x -= OBJSPEED;
+
     }
   }
+
 
   //drawing the ground
   CTX.fillStyle = colours.groundColour;
   CTX.fillRect(0, FLOORHEIGHT, CVS.width, CVS.height / 4);
+
 
   // drawing the character
   CTX.beginPath();
   CTX.arc(character.x, character.y, character.radius, 0, Math.PI * 2);
   CTX.fillStyle = colours.characterColour;
   CTX.fill();
+
 
   // writing the score
   CTX.font = "20px Arial";
@@ -291,6 +396,7 @@ function draw() {
   CTX.fillText("Score: ", CVS.width - 170, 30);
   CTX.fillText(score, CVS.width - 100, 30);
 
+
   // writing the score
   CTX.font = "20px Arial";
   CTX.strokeStyle = "white";
@@ -300,9 +406,11 @@ function draw() {
   CTX.fillText("Lives: ", 30, 30);
   CTX.fillText(lives, 100, 30);
 
-// if the spacebar has been pressed and not released, the character jumps
+
+  // if the spacebar has been pressed and not released, the character jumps
   if (controller.space && character.jumping === false) {
 
+    jump.play();
     character.jumping = true;
     character.yVelocity = JUMPSTRENGTH;
 
@@ -343,10 +451,13 @@ function draw() {
 };
 
 
+
 function died(){
+
 
   CTX.fillStyle = colours.skyColour;
   CTX.fillRect(0, 0, CVS.width, CVS.height);
+
 
   // drawing pipes
   for (var i = 0; i < pipes.length; i++) {
@@ -359,6 +470,7 @@ function died(){
 
   }
 
+
   // drawing the coins
   for (var i = 0; i < coins.length; i++) {
 
@@ -368,6 +480,7 @@ function died(){
     CTX.fill();
 
   }
+
 
   // drawing the obstacles
   for (var i = 0; i < obstacles.length; i++) {
@@ -385,18 +498,14 @@ function died(){
   CTX.fillStyle = "black";
   CTX.fillText("Scoreboard",  CVS.width / 2 - 110, scoreboardYPos - 40);
 
-  // scores
+  // writing the scores
   for (var i = 0; i < scoreboard.length; i++) {
 
     CTX.font = "20px Arial";
     CTX.strokeStyle = "white";
-    CTX.strokeText(i + 1,  CVS.width / 2 - 100, scoreboardYPos);
-    CTX.strokeText(":",  CVS.width / 2 - 90, scoreboardYPos);
-    CTX.strokeText(scoreboard[i], CVS.width / 2, scoreboardYPos);
+    CTX.strokeText((i + 1) + ":   " + scoreboard[i],  CVS.width / 2 - 50, scoreboardYPos);
     CTX.fillStyle = "black";
-    CTX.fillText(i + 1,  CVS.width / 2 - 100, scoreboardYPos);
-    CTX.fillText(":",  CVS.width / 2 - 90, scoreboardYPos);
-    CTX.fillText(scoreboard[i],  CVS.width / 2, scoreboardYPos);
+    CTX.fillText((i + 1) + ":   " + scoreboard[i],  CVS.width / 2 - 50, scoreboardYPos);
     scoreboardYPos += 30;
 
   }
@@ -418,10 +527,17 @@ function died(){
   CTX.fillStyle = colours.characterColour;
   CTX.fill();
 
+  if (character.y + character.radius < FLOORHEIGHT) {
+    character.yVelocity += GRAVITY; // gravity changing y velocity
+    character.y += character.yVelocity; // y velocity changing the characters y
+  }
+
 }
+
 
 // reset function for starting up again after you die
 function reset() {
+
   lives = 3;
   pipes.splice(0, pipes.length);
   coins.splice(0, coins.length);
@@ -431,36 +547,43 @@ function reset() {
   makeCoin();
   makeObstacle();
   gameRunning = true;
-}
 
-var scoreboardYPos; // making the y position variable for the scoreboard
+}
 
 // loop function to allow the game to stop and start
 function loop() {
 
   if (gameRunning === false && controller.rKey){ // if the r key is pressed while the game has stopped
+
     reset();
+
   }
 
   if (gameRunning) {
+
     draw(); // if the game is running then call draw
+
   } else if (gameRunning === false){
+
+    died(); // if the game isn't running call the died function
     scoreboardYPos = FLOORHEIGHT - 200;
-    died(); // if it isn't running call the died function
   }
 
   window.requestAnimationFrame(loop); // recalling the function to make it a loop
 }
 
 
+
 // event listeners for the controller
 window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
+
 
 // making a pipe and coin and an obstacle so that they are ready for the game
 makePipe();
 makeCoin();
 makeObstacle();
+
 
 // calling the loop function for the first time to start the loop
 window.requestAnimationFrame(loop);
